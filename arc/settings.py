1ca069988ea073f9a3c01545525515471a303c68
K. Sannidhya Shukla <feynhat@gmail.com>
2019-04-10 23:58:58 +0530

    settings

--- arc/settings.py
+++ arc/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '10.10.0.79']
 
@@ -81,7 +81,7 @@ DATABASES = {
         'PORT': '',
         'NAME': 'db',
         'USER': 'admin',
-        #'PASSWORD': 'foobar',
+        'PASSWORD': 'foobar',
     }
 }
 
