3541d59086ae044e2b5556578c06691cbd4c4fa4
Ayush Singh <ayush.singh@niser.ac.in>
2020-02-22 00:54:55 +0530

    added email notifications

---
+++ arc/local_settings_example.py
@@ -0,0 +1,34 @@
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '10.0.2.35']
+
+ADMINS = []
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Media and static files for deployment
+MEDIA_URL = '/arc/media/'
+STATIC_URL = '/arc/static/'
+
+SECRET_KEY = 'somethingrandomlygenerateddefinitelynotthis'
+EMAIL_HOST = ''
+EMAIL_PORT = '465'
+EMAIL_HOST_USER = ''
+SERVER_EMAIL = 'NISER Archive'
+#You shouldn't be reading this
+EMAIL_HOST_PASSWORD = ''
+#But honestly though, I don't care if this gets hacked. Its not my personal email.
+EMAIL_USE_SSL = True
+EMAIL_SUBJECT_PREFIX = ''
