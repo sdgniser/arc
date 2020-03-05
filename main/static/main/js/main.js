c9f84e83a6c89964ba26430c9b36e07ae4340360
Jyotirmaya Shivottam <31299799+JeS24@users.noreply.github.com>
2020-03-05 22:16:35 +0530

    Added darkmode.js

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -26,3 +26,20 @@ $(document).ready(function() {
         $(this).html(converter.makeHtml(t));
     });
 });
+
+// For DarkMode.js
+var options = {
+    bottom: '6px', // default: '32px'
+    right: '30px', // default: '32px'
+    left: 'unset', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#24292E',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+}
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
