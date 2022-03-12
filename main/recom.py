8cfcfef1d82a5f75010d7571f7f86618285d4d11
Rahul Vishwakarma <53302959+rahul3613@users.noreply.github.com>
2022-03-13 01:53:08 +0530

    Used numpydoc for docstring

--- main/recom.py
+++ main/recom.py
@@ -8,15 +8,15 @@ import pandas as pd
 
 def start():
     """
-    Start Function.
-
     Function to check if the file to store the user file access data
     already exists or not, if not then create one.
 
-    Parameters:
+    Parameters
+    ----------
     None
 
-    Returns:
+    Returns
+    -------
     None
     """
     if not os.path.exists("recom_data.csv"):
@@ -33,16 +33,18 @@ start()  # Call the start function
 
 def get_recom(user_id):
     """
-    Get Recommendations Function.
-
     Function to get the list of recommended files for a given user.
 
-    Parameters:
-    user_id (int/str): Unique ID of the user
+    Parameters
+    ----------
+    user_id : int, str
+        Unique ID of the user.
 
-    Returns:
-    list: A list of recommended files sorted in descending order of priority.
-          With max length of list equals to 10.
+    Returns
+    -------
+    list
+        A list of recommended files sorted in descending order of priority.
+        With max length of list equals to 10.
     """
 
     user_id = str(user_id)
@@ -68,17 +70,18 @@ def get_recom(user_id):
 
 def update(file, user_id):
     """
-    Update Funaction.
-
     Function to update the list of files accessed by user.
 
-    Parameters:
-    file (str): Name of the file being accessed by user.
-    user_id (int/str): Unique ID of the user accessing the file.
+    Parameters
+    ----------
+    file : str
+        Name of the file being accessed by user.
+    user_id : int, str
+        Unique ID of the user accessing the file.
 
-    Returns:
+    Returns
+    -------
     None
-
     """
 
     user_id = str(user_id)
@@ -89,7 +92,7 @@ def update(file, user_id):
         df[user_id] = [0] * len(df)
 
     if file not in df.index:
-        df.loc[file] = [0]*len(df.columns)
+        df.loc[file] = [0] * len(df.columns)
 
     df.loc[[file], [user_id]] = 10
 
